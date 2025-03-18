24a28ac9afcfd2c0ceac17ebd5ccbd65e18b71de
iluhinzoh1 <ilyanovikov007@mail.ru>
2025-03-18 16:28:38 +0300

    добавил кастомный login

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping()
+    public String redirectToRolePage() {
+        return "login";
+    }
+}
