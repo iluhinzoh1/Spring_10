34bda7568c52284d8d607075d7d6f02d3bca0ae5
iluhinzoh1 <ilyanovikov007@mail.ru>
2025-03-26 10:50:38 +0300

    поправил

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
@@ -1,10 +1,7 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
